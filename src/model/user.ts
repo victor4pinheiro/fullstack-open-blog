764c5ae5566dff84c6cd42384b260b75d34ddfdd
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-02-13 15:56:34 -0300

    db: add user schema

---
+++ src/model/user.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import UserInterface from "../interfaces/user";
+
+mongoose.set("strictQuery", false);
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    minLength: 3,
+    required: [true, "username required"],
+  },
+  name: {
+    type: String,
+    required: [true, "name required"],
+  },
+  password: {
+    type: String,
+    minLength: 3,
+    required: [true, "password required"],
+  },
+});
+
+userSchema.set("toJSON", {
+  transform(_doc, ret: UserInterface) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.password;
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
