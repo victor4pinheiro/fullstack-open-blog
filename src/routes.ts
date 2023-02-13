46600f79bc07dff323ceab3f77665a485aae1ae6
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-02-13 15:55:58 -0300

    feat: add get and post users

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 import BlogController from "./controller/blog";
+import UserController from "./controller/user";
 
-const baseURL = "/api/blogs";
+enum baseURL {
+  blog = "/api/blogs",
+  user = "/api/users",
+}
 const router = Router();
 
-router.get(baseURL, BlogController.getAll);
-router.post(baseURL, BlogController.create);
-router.put(`${baseURL}/:id`, BlogController.update);
-router.delete(`${baseURL}/:id`, BlogController.remove);
+router.get(baseURL.blog, BlogController.getAll);
+router.post(baseURL.blog, BlogController.create);
+router.put(`${baseURL.blog}/:id`, BlogController.update);
+router.delete(`${baseURL.blog}/:id`, BlogController.remove);
+
+router.get(baseURL.user, UserController.getAll);
+router.post(baseURL.user, UserController.create);
 
 export default router;
