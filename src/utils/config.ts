d357774faf072344500a516263fa64593c5faab0
victor4pinheiro <victor.pinheiro.soft@gmail.com>
2023-02-23 16:12:29 -0300

    env: add SECRET environment

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,5 +4,6 @@ dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const PORT = process.env.PORT;
+const SECRET = process.env.SECRET || "teste";
 
-export { PORT, MONGODB_URI };
+export { PORT, MONGODB_URI, SECRET };
